9709c0e6e4345bfcf97e4ade9260aac122c96309
Fernando Ashur <ferchoashur@gmail.com>
2022-07-04 16:08:35 -0300

    Primer Draft BlogCoder

---
+++ Blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import AddPostView, HomeView, ArticleDetailView, UpdatePostView,DeletePostView
+
+urlpatterns = [
+    #path('',views.home, name='home'),
+    path('', HomeView.as_view(),name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'), 
+    path('post', AddPostView.as_view(),name='add_post'),
+    path('article/edit/<int:pk>',UpdatePostView.as_view(),name='update'),
+    path('article/edit/<int:pk>/delete',DeletePostView.as_view(),name='delete'),
+]
